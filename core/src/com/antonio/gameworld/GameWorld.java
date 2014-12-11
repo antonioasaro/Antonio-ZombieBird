7244b747570181f8f0fe055c248dcb436def1b59
antonioasaro <antonio_asaro@hotmail.com>
2014-12-10 19:46:17 -0500

    End of day 4

--- core/src/com/antonio/gameworld/GameWorld.java
+++ core/src/com/antonio/gameworld/GameWorld.java
@@ -1,11 +1,21 @@
 package com.antonio.gameworld;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Rectangle;
 
 public class GameWorld {
 
-	public void update(float delta) {
-		Gdx.app.log("GameWorld", "update");
-	}
+    private Rectangle rect = new Rectangle(0, 0, 17, 12);
 
+    public void update(float delta) {
+        Gdx.app.log("GameWorld", "update");
+        rect.x++;
+        if (rect.x > 137) {
+            rect.x = 0;
+        }
+    }
+
+    public Rectangle getRect() {
+        return rect;
+    }
 }
